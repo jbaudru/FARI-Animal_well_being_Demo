f813c7750ad7a0f0b412d1793b14510bf932ef33
Julien Baudru <julien.baudru@gmail.com>
2022-10-31 16:02:26 +0100

    Select difficulty level

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 from flask import Flask, redirect, url_for, request, render_template
 import json
 
+global difficulty 
+
 app = Flask(__name__, template_folder='templateFiles', static_folder='staticFiles')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def home():
    return render_template('rules.html')
 
-@app.route('/play')
+
+@app.route('/play', methods=['GET', 'POST'])
 def play():
     data = None
     with open("staticFiles/data/ads.json") as json_file:
@@ -15,7 +19,22 @@ def play():
 
     return render_template('play.html', ads=data["ads"])
 
-@app.route('/leaderboard')
+
+@app.route('/getmethod/<jsdata>')
+def get_javascript_data(jsdata):
+    global difficulty
+    difficulty = 0
+    if(jsdata=="eazy"):
+        difficulty = 0
+    elif(jsdata=="normal"):
+        difficulty = 1
+    elif(jsdata=="hard"):
+        difficulty = 2
+    print("Level of difficuly selected: ", difficulty)
+    return jsdata
+
+
+@app.route('/leaderboard', methods=['GET', 'POST'])
 def leaderboard():
     data = None
     with open("staticFiles/data/leaderboard.json") as json_file:
